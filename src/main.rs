e95e24f934bedad6128851f20f44ef224b0bf34f
Avarel <avarelpm@gmail.com>
2019-05-05 21:09:34 -0500

    Fix backspace bug, generalized some functions

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,14 @@ use console::{Term, Key};
 
 fn main() -> io::Result<()> {
     let mut prompt = MultilineTerm::stdout();
-    // prompt.buffers = vec![
-    //     "hello there".to_owned(),
-    //     "howtheheck are you doing".to_owned(),
-    //     "memes".to_owned()
-    // ];
-    // prompt.cursor = Cursor {
-    //     line: 2,
-    //     index: 3,
-    // };
-    // prompt.prompt = Some(("  in > ".to_owned(), " ... > ".to_owned()));
+    prompt.buffers = vec![
+        "hello there".to_owned(),
+        "howtheheck are you doing".to_owned(),
+        "memes".to_owned()
+    ];
+    prompt.line = 2;
+    prompt.index = 3;
+    prompt.prompt = Some(("  in > ".to_owned(), " ... > ".to_owned()));
 
     let result = prompt.read_prompt()?;
     println!("{}", result);
@@ -22,9 +20,10 @@ fn main() -> io::Result<()> {
 
 struct MultilineTerm {
     term: Term,
-    /// Buffers to draw
+    empty_padding: usize,
     pub buffers: Vec<String>,
-    pub cursor: Cursor,
+    pub line: usize,
+    pub index: usize,
     pub prompt: Option<(String, String)>,
 }
 
@@ -33,7 +32,9 @@ impl MultilineTerm {
         Self {
             term: Term::stdout(),
             buffers: Vec::new(),
-            cursor: Cursor { line: 1, index: 0 },
+            empty_padding: 0,
+            line: 1,
+            index: 0,
             prompt: None
         }
     }
@@ -42,13 +43,23 @@ impl MultilineTerm {
         Self {
             term: Term::stderr(),
             buffers: Vec::new(),
-            cursor: Cursor { line: 1, index: 0 },
+            empty_padding: 0,
+            line: 1,
+            index: 0,
             prompt: None
         }
     }
 
-    pub fn set_prompt(&mut self, prompt: Option<(String, String)>) {
-        self.prompt = prompt
+    fn current_line_len(&self) -> usize {
+        self.current_line().len()
+    }
+
+    fn current_line(&self) -> &String {
+        &self.buffers[self.line - 1]
+    }
+
+    fn current_line_mut(&mut self) -> &mut String {
+        &mut self.buffers[self.line - 1]
     }
 
     pub fn read_prompt(mut self) -> io::Result<String> {
@@ -56,75 +67,61 @@ impl MultilineTerm {
         loop {
             match self.term.read_key()? {
                 Key::ArrowDown => {
-                    if self.cursor.line < self.buffers.len() {
-                        self.move_cursor_down(1)?;
-                        self.cursor.line += 1;
+                    if self.line < self.buffers.len() {
+                        self.line = self.move_cursor_down(1)?;
 
-                        let len = self.buffers[self.cursor.line - 1].len();
-                        if self.cursor.index > len {
-                            self.move_cursor_left(self.cursor.index - len)?;
-                            self.cursor.index = len;
+                        if self.index > self.current_line_len() {
+                            self.index = self.move_cursor_to_end()?;
                         }
                     }
                 }
                 Key::ArrowUp => {
-                    if self.cursor.line > 1 {
-                        self.move_cursor_up(1)?;
-                        self.cursor.line -= 1;
+                    if self.line > 1 {
+                        self.line = self.move_cursor_up(1)?;
 
-                        let len = self.buffers[self.cursor.line - 1].len();
-                        if self.cursor.index > len {
-                            self.move_cursor_left(self.cursor.index - len)?;
-                            self.cursor.index = len;
+                        if self.index > self.current_line_len() {
+                            self.index = self.move_cursor_to_end()?;
                         }
                     }
                 }
                 Key::ArrowLeft => {
-                    if self.cursor.index > 0 {
-                        self.move_cursor_left(1)?;
-                        self.cursor.index -= 1;
-                    } else if self.cursor.line > 1 {
-                        self.move_cursor_up(1)?;
-                        self.cursor.line -= 1;
-                        let len = self.buffers[self.cursor.line - 1].len();
-                        self.move_cursor_right(len)?;
-                        self.cursor.index = len;
+                    if self.index > 0 {
+                        self.index = self.move_cursor_left(1)?;
+                    } else if self.line > 1 {
+                        self.line = self.move_cursor_up(1)?;
+                        self.index = self.move_cursor_to_end()?;
                     }
                 }
                 Key::ArrowRight => {
-                    let len = self.buffers[self.cursor.line - 1].len();
-                    if self.cursor.index < len {
-                        self.move_cursor_right(1)?;
-                        self.cursor.index += 1;
-                    } else if self.cursor.line < self.buffers.len() {
-                        self.move_cursor_left(len)?;
-                        self.move_cursor_down(1)?;
-                        self.cursor.index = 0;
-                        self.cursor.line += 1;
+                    let len = self.current_line().len();
+                    if self.index < len {
+                        self.index = self.move_cursor_right(1)?;
+                    } else if self.line < self.buffers.len() {
+                        self.line = self.move_cursor_down(1)?;
+                        self.index = self.move_cursor_to_start()?;
                     }
                 }
-                Key::Char(c) if c == '\x7f' => {
-                    if self.cursor.index > 0 {
+                Key::Char('\x7f') => {
+                    if self.index > 0 {
                         self.delete_char_before_cursor();
-                        self.cursor.index -= 1;
+                        self.index -= 1;
                         self.redraw()?;
-                    } else if self.cursor.line > 1 {
+                    } else if self.line > 1 {
                         self.draw_clear()?;
-                        self.term.write_line("")?;
-
-                        let cbuf = self.buffers.remove(self.cursor.line - 1);
-                        self.cursor.line -= 1;
+                        self.empty_padding += 1;
                         
-                        let nbuf = &mut self.buffers[self.cursor.line - 1];
-                        self.cursor.index = nbuf.len();
-                        nbuf.push_str(&cbuf);
+                        let cbuf = self.buffers.remove(self.line - 1);
+                        self.line -= 1;
                         
-                        self.redraw()?;
+                        self.index = self.current_line_len();
+                        self.current_line_mut().push_str(&cbuf);
+                        
+                        self.draw()?;
                     }
                 }
                 Key::Char(c) => {
                     self.insert_char_at_cursor(c);
-                    self.cursor.index += 1;
+                    self.index += 1;
                     self.redraw()?;
                 }
                 Key::Escape => {
@@ -132,17 +129,24 @@ impl MultilineTerm {
                     break
                 }
                 Key::Enter => {
-                    if self.cursor.line == self.buffers.len() && self.buffers[self.cursor.line - 1].len() == 0 {
-                        self.buffers.remove(self.cursor.line - 1);
+                    if self.line == self.buffers.len() && self.current_line_len() == 0 {
+                        self.buffers.remove(self.line - 1);
                         break
                     } else {
-                        let cbuf = &mut self.buffers[self.cursor.line - 1];
-                        let nbuf = cbuf.split_off(self.cursor.index);
-                        self.buffers.insert(self.cursor.line, nbuf);
-                        self.cursor.index = 0;
-                        self.cursor.line += 1;
+                        let cursor_idx = self.index;
+                        let cbuf = self.current_line_mut();
+                        let nbuf = cbuf.split_off(cursor_idx);
+                        self.buffers.insert(self.line, nbuf);
+                        self.index = 0;
+                        self.line += 1;
+                        
                         self.move_cursor_to_bottom()?;
-                        self.term.write_line("")?;
+                        if self.empty_padding == 0 {
+                            self.term.write_line("")?;
+                        } else {
+                            self.empty_padding -= 1;
+                        }
+
                         self.redraw()?;
                     }
                 }
@@ -164,8 +168,9 @@ impl MultilineTerm {
     }
 
     fn delete_char_before_cursor(&mut self) {
-        let buf = &mut self.buffers[self.cursor.line - 1];
-        buf.remove(self.cursor.index - 1);
+        let idx = self.index;
+        let buf = self.current_line_mut();
+        buf.remove(idx - 1);
     }
 
     fn insert_char_at_cursor(&mut self, c: char) {
@@ -173,70 +178,100 @@ impl MultilineTerm {
             self.buffers.push(String::new());
         }
 
-        let buf = &mut self.buffers[self.cursor.line - 1];
-        buf.insert(self.cursor.index, c);
+        let idx = self.index;
+        let buf = self.current_line_mut();
+        buf.insert(idx, c);
     }
 
-    fn draw(&mut self) -> io::Result<()> {
+    fn draw(&self) -> io::Result<()> {
         if self.buffers.len() == 0 { return Ok(()) }
+
+        for _ in 0..self.empty_padding {
+            self.term.write_line("")?;
+        }
         
         for i in 0..self.buffers.len() - 1 {
-            if let Some((first, follow)) = &self.prompt {
-                if i == 0 {
-                    self.term.write_str(first)?;
-                } else {
-                    self.term.write_str(follow)?;
-                }
-            }
+            self.draw_prompt(i == 0)?;
             self.term.write_line(&self.buffers[i])?;
         }
 
         let last = &self.buffers[self.buffers.len() - 1];
         let last_len = last.len();
-        if let Some((_, follow)) = &self.prompt {
-            self.term.write_str(follow)?;
-        }
+        self.draw_prompt(self.buffers.len() == 1)?;
         self.term.write_str(last)?;
 
         self.move_cursor_left(last_len)?;
-        self.term.move_cursor_up(self.buffers.len() - self.cursor.line)?;
-        self.term.write_str(&self.buffers[self.cursor.line - 1][0..self.cursor.index])?;
+        self.term.move_cursor_up(self.buffers.len() - self.line)?;
+        self.term.write_str(&self.current_line()[0..self.index])?;
         Ok(())
     }
 
-    fn redraw(&mut self) -> io::Result<()> {
+    fn draw_prompt(&self, draw_first: bool) -> io::Result<()> {
+        if let Some((first, follow)) = &self.prompt {
+            if draw_first {
+                self.term.write_str(first)?;
+            } else {
+                self.term.write_str(follow)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn redraw(&self) -> io::Result<()> {
         self.draw_clear()?;
         self.draw()
     }
 
-    fn move_cursor_to_bottom(&self) -> io::Result<()> {
-        self.term.move_cursor_down(self.buffers.len() - self.cursor.line + 1)
+    fn move_cursor_to_bottom(&self) -> io::Result<usize> {
+        self.term.move_cursor_down(self.buffers.len() - self.line + 1)?;
+        Ok(self.buffers.len())
     }
 
-    fn move_cursor_up(&mut self, n: usize) -> io::Result<()> {
-        self.term.move_cursor_up(n)
+    fn move_cursor_up(&self, n: usize) -> io::Result<usize> {
+        self.term.move_cursor_up(n)?;
+        Ok(self.line - 1)
     }
 
-    fn move_cursor_down(&mut self, n: usize) -> io::Result<()> {
-        self.term.move_cursor_down(n)
+    fn move_cursor_down(&self, n: usize) -> io::Result<usize> {
+        self.term.move_cursor_down(n)?;
+        Ok(self.line + 1)
     }
 
-    fn move_cursor_left(&mut self, n: usize) -> io::Result<()> {
+    fn move_cursor_to_end(&self) -> io::Result<usize> {
+        let len = self.current_line().len();
+        if self.index > len {
+            self.move_cursor_left(self.index - len)?;
+        } else if self.index < len {
+            self.move_cursor_right(len - self.index)?;
+        }
+        Ok(len)
+    }
+
+    fn move_cursor_to_start(&self) -> io::Result<usize> {
+        self.move_cursor_left(self.index)?;
+        Ok(0)
+    }
+
+    fn move_cursor_left(&self, n: usize) -> io::Result<usize> {
         for _ in 0..n {
             self.term.write_str("\x08")?;
         }
-        Ok(())
+        if self.index == 0 {
+            Ok(0)
+        } else {
+            Ok(self.index - 1)
+        }
     }
 
-    fn move_cursor_right(&mut self, n: usize) -> io::Result<()> {
-        self.term.write_str(&self.buffers[self.cursor.line - 1][self.cursor.index..self.cursor.index + n])?;
-        Ok(())
+    fn move_cursor_right(&self, n: usize) -> io::Result<usize> {
+        self.term.write_str(&self.current_line()[self.index..self.index + n])?;
+        Ok(self.index + 1)
     }
 
     fn draw_clear(&self) -> io::Result<()> {
         self.move_cursor_to_bottom()?;
         self.term.clear_line()?;
-        self.term.clear_last_lines(self.buffers.len() - 1)?;
+        self.term.clear_last_lines(self.buffers.len() - 1 + self.empty_padding)?;
         Ok(())
     }
 }
