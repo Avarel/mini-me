eb199864fec8ce96c0ee179948a15018ce1c5b9b
Avarel <avarelpm@gmail.com>
2019-11-14 01:01:50 -0800

    Fix examples, prep for crossterm change

--- examples/gutter.rs
+++ examples/gutter.rs
@@ -14,16 +14,16 @@ use multiline_console::renderer::{LazyRenderer, FullRenderer};
 //      )
 
 fn main() -> io::Result<()> {
-    println!("{} | Write something cool!", style(" >>> ").black().on_green());
+    println!("        Write something cool!");
     let term = multiline_console::MultilineTerm::builder()
         // Create a lazy renderer (more efficient) that prints out the gutter on the side.
         .renderer(LazyRenderer::wrap(FullRenderer::with_gutter(move |i, term| {
             // Signal that you're supposed to ENTER when the buffer is
             // empty/has a length of zero in order to submit the response.
             if term.buffers().is_empty() || i + 1 == term.buffers().len() && term.buffers().last().unwrap().len() == 0 {
-                format!("enter | ")
+                format!("{} ", style(" enter ").on_green())
             } else {
-                format!("{:>5} | ", i + 1)
+                format!("{} ", style(format!(" {:>5} ", i + 1)).on_blue())
             }
         })))
         // Build the prompt.
